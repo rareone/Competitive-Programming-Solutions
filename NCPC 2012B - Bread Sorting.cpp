41397df61d8b2862fe826db2611f42b460c13efa
rareone <rareone0602@me.com>
2016-10-16 03:05:22 +0800

    Update NCPC 2012B - Bread Sorting.cpp

--- NCPC 2012B - Bread Sorting.cpp
+++ NCPC 2012B - Bread Sorting.cpp
@@ -1,37 +1,44 @@
 #include <iostream>
-#define MAXN  100010
+#define MAXN  100000
 int A[MAXN],B[MAXN],S[MAXN];
+
+
 
 //小心溢位，之前因為這樣錯死
 //用mergesort找出有幾個inversion
-//奇數偶數互不可交換
-//前一題水所以沒放
+//解決溢位位問題之後AC
 
 long long int countinversion(int *A,int begin,int end){//A[begin,end)
     if(begin+1==end)return 0;
     long long int a=countinversion(A,begin, (begin+end)/2);
     long long int b=countinversion(A,(begin+end)/2, end);
-    int keyb=begin,keye=(begin+end)/2;
-    long long int c=0,hold=begin,it=begin;
+    int keyb=begin,keye=(begin+end)/2,hold=begin;
+    long long int c=0;
     while(keyb!=(begin+end)/2 && keye!=end){
         if(A[keyb]>A[keye]){
-            while(it!=(begin+end)/2 && A[it]>A[keye])++it;
-            c+=it-begin;
+            c+=(begin+end)/2-keyb;
             S[hold++]=A[keye++];
         }else S[hold++]=A[keyb++];
     }
     while(keyb!=(begin+end)/2) S[hold++]=A[keyb++];
     while(keye!=end) {
-        while(it!=(begin+end)/2 && A[it]>A[keye])++it;
-        c+=it-begin;
         S[hold++]=A[keye++];
     }
     for(int i=begin;i<end;++i) A[i]=S[i];
     return a+b+c;
 }
+int getInvCount(int arr[], int n)
+{
+    int inv_count = 0;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i+1; j < n; j++)
+            if (arr[i] > arr[j])
+                inv_count++;
+    
+    return inv_count;
+}
 
 int main(){
-    
     int n;scanf("%d",&n);
     for(int i=0;i<n;++i) scanf("%d",&A[i]);
     for(int i=0;i<n;++i) scanf("%d",&B[i]);
