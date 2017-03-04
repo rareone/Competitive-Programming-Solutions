817c386a3645fa2261b75e1c2c81d59c15186463
rareone <rareone0602@me.com>
2017-03-04 20:34:43 +0800

    解難題好爽喔

---
+++ Weekly Training Farm 16 B3.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#define FL(i,j,k) for(int i=0;i<k;++i)
+using namespace std;
+const int log=20;
+long long A[1<<log]={0},B[1<<log]={0};
+
+void XORtransform(long long *P, int k=log,bool inverse=0) {
+    for (int len = 1; 2 * len <= (1<<k); len <<= 1)
+        for (int i = 0; i < (1<<k); i += 2 * len)
+            for (int j = 0; j < len; ++j) {
+                long long u = P[i + j] , v = P[i + len + j];
+                P[i + j] = u + v , P[i + len + j] = u - v;
+            }
+    if (inverse) for (int i = 0; i < (1<<k); ++i) P[i] = P[i] / (1<<k);
+}
+
+void ANDtransform(long long *P, int k=log,bool inv=0) {
+    for (int len = 1; 2 * len <= (1<<k); len <<= 1)
+        for (int i = 0; i < (1<<k); i += 2 * len)
+            for (int j = 0; j < len; ++j) {
+                long long u = P[i + j] , v = P[i + len + j];
+                if (!inv) P[i + j] = v , P[i + len + j] = u + v;
+                else {
+                    P[i + j] = -u + v , P[i + len + j] = u;
+                }
+            }
+}
+
+void ORtransform(long long *P, int k=log,bool inv=0) {
+    for (int len = 1; 2 * len <= (1<<k); len <<= 1)
+        for (int i = 0; i < (1<<k); i += 2 * len)
+            for (int j = 0; j < len; ++j) {
+                long long u = P[i + j] , v = P[i + len + j];
+                if (!inv) P[i + j] = u , P[i + len + j] = u + v;
+                else {
+                    P[i + j] = u , P[i + len + j] = v - u;
+                }
+            }
+    
+}
+void findpdt(long long *P, int k=log,bool inv=0){
+    for (int len = 1; 2 * len <= (1<<k); len <<= 1)
+        for (int i = 0; i < (1<<k); i += 2 * len)
+            for (int j = 0; j < len; ++j) {
+                long long u = P[i + j] , v = P[i + len + j];
+                if (!inv) P[i + j] = u , P[i + len + j] = u + v;
+                else {
+                    P[i + j] = v , P[i + len + j] = v - u;
+                }
+            }
+}
+void findalong(long long *P, int k=log){
+    for (int len = 1; 2 * len <= (1<<k); len <<= 1)
+        for (int i = 0; i < (1<<k); i += 2 * len)
+            for (int j = 0; j < len; ++j) {
+                long long u = P[i + j] , v = P[i + len + j];
+                P[i + j] = u + v , P[i + len + j] = v;
+            }
+}
+long long int Ans[1<<20];
+int main(){
+    ios_base::sync_with_stdio(false);
+    int n,m;cin>>n>>m;string c;
+    FL(i, 0, n){
+        int a=0;
+        cin>>c;
+        FL(i, 0, 20){
+            if(c[i]-'0')a+=(1<<i);
+        }
+        ++A[a],++B[a];
+    }
+    findpdt(A),findalong(B);
+    FL(i, 0, 1<<20)Ans[i]=A[i]*A[i];
+    findpdt(Ans,20,true);
+    FL(i, 0, m){
+        int a=0;
+        cin>>c;
+        FL(j, 0, 20){
+            if(c[j]-'0')a+=(1<<j);
+        }
+        cout<<(Ans[a]-B[a])/2<<'\n';
+    }
+}
