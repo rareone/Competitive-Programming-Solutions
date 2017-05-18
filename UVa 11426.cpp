dd52b79e592fd11f2c41d515aa260b01f1fe94d6
rareone <rareone0602@me.com>
2017-05-18 09:09:09 +0800

    數論
    用了一點類莫比烏斯反演
    又不太像

---
+++ UVa 11426.cpp
@@ -0,0 +1,137 @@
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#define SZ(X) ((X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (LL I = 0; I < (N); ++I)
+#define REPP(I, A, B) for (LL I = (A); I < (B); ++I)
+#define RI(X) scanf("%lld", &(X))
+#define RII(X, Y) scanf("%lld%lld", &(X), &(Y))
+#define RIII(X, Y, Z) scanf("%lld%lld%lld", &(X), &(Y), &(Z))
+#define DRI(X) LL (X); scanf("%lld", &X)
+#define DRII(X, Y) LL X, Y; scanf("%lld%lld", &X, &Y)
+#define DRIII(X, Y, Z) LL X, Y, Z; scanf("%lld%lld%lld", &X, &Y, &Z)
+#define RS(X) scanf("%s", (X))
+#define CASET int ___T, case_n = 1; scanf("%d ", &___T); while (___T-- > 0)
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define PII pair<int,int>
+#define VI vector<int>
+#define VL vector<long long>
+#define VPII vector<pair<int,int> >
+#define PLL pair<long long,long long>
+#define VPLL vector<pair<long long,long long> >
+#define F first
+#define S second
+#define IOS ios_base::sync_with_stdio(false);cin.tie(0);
+typedef long long LL;
+using namespace std;
+
+LL phi[4000001]={0,0,0};
+
+int main(){
+    for(int i=2;i<4000001;++i){
+        if(phi[i]==0){
+            for(int j=i;j<4000001;j+=i){
+                if(!phi[j])phi[j]=1;
+                phi[j]*=i-1;
+                LL cur=j/i;
+                while(cur%i==0)phi[j]*=i,cur/=i;
+            }
+        }
+    }
+    LL n;
+    while(~RI(n) && n){
+        LL ans=0;
+        for(LL j=1;j<=n;++j){
+            for(LL i=max(2ll,j);i<=n;i+=j)ans+=phi[i/j]*j;
+        }
+        printf("%lld\n",ans);
+    }
+}
